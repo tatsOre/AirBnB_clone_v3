b087e69363c26de40a54f973197eae7dc3754594
luis sanjuan <luiscode92@gmail.com>
2020-09-10 00:35:01 -0500

    update

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.review import Review
-
+from models.place import Place
 
 @app_views.route('/places/<place_id>/reviews', methods=["GET"],
                  strict_slashes=False)
