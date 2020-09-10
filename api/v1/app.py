9a28cce0a4adac3086af43ef549473b07e4a64a5
tatsOre <tatianaorezap@gmail.com>
2020-09-09 21:17:52 -0500

    Update - HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,13 @@
 Module that starts a Flask API
 """
 from flask import Flask, jsonify, Blueprint
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
