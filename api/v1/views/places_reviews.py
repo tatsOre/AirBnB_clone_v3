2301e3b355c95e32da7e221cef9e5d9d23726378
luis sanjuan <luiscode92@gmail.com>
2020-09-09 22:27:49 -0500

    pep8 fixed

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -8,7 +8,7 @@ from models import storage
 from models.review import Review
 
 
-@app_views.route('/places/<place_id>/reviews', strict_slashes=False)
+@app_views.route('/api/v1/places/<place_id>/reviews', strict_slashes=False)
 def all_reviews(place_id):
     """ retrieve list of all Review objects """
     all_reviews = []
@@ -20,7 +20,7 @@ def all_reviews(place_id):
     return jsonify(all_reviews)
 
 
-@app_views.route('/reviews/<review_id>', strict_slashes=False)
+@app_views.route('/api/v1/reviews/<review_id>', strict_slashes=False)
 def retrieve_review(review_id):
     """ retrieve a particular Review """
     review = storage.get('Review', review_id)
@@ -29,7 +29,7 @@ def retrieve_review(review_id):
     abort(404)
 
 
-@app_views.route('/reviews/<review_id>', methods=['DELETE'],
+@app_views.route('/api/v1/reviews/<review_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_review(review_id):
     """ delete a Review """
@@ -41,7 +41,7 @@ def delete_review(review_id):
     abort(404)
 
 
-@app_views.route('/places/<place_id>/reviews', methods=['POST'],
+@app_views.route('/api/v1/places/<place_id>/reviews', methods=['POST'],
                  strict_slashes=False)
 def create_review(place_id):
     """ create a Review """
@@ -63,7 +63,7 @@ def create_review(place_id):
     return new_review.to_dict(), 201
 
 
-@app_views.route('/reviews/<review_id>', methods=['PUT'],
+@app_views.route('/api/v1/reviews/<review_id>', methods=['PUT'],
                  strict_slashes=False)
 def update_review(review_id):
     """ update a Review """
