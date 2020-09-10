3a8b32c10972baabd03197f6568b0e18f09564c9
luis sanjuan <luiscode92@gmail.com>
2020-09-09 22:13:11 -0500

    added places review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-New view for link between Place and Amenity objects that handles default
-Restful API actions
+New view for Review objects that handles default Restful API actions
 """
 from flask import Flask, jsonify, abort, request
 from api.v1.views import app_views
@@ -9,48 +8,74 @@ from models import storage
 from models.review import Review
 
 
-@app_views.route('/api/v1/places/<place_id>/amenities',
+@app_views.route('/places/<place_id>/reviews', strict_slashes=False)
+def all_reviews(place_id):
+    """ retrieve list of all Review objects """
+    all_reviews = []
+    if not storage.get('Place', place_id):
+        abort(404)
+    for review in storage.all('Review').values():
+        if place_id == review.to_dict()['place_id']:
+            all_reviews.append(review.to_dict())
+    return jsonify(all_reviews)
+
+
+@app_views.route('/reviews/<review_id>', strict_slashes=False)
+def retrieve_review(review_id):
+    """ retrieve a particular Review """
+    review = storage.get('Review', review_id)
+    if review:
+        return review.to_dict()
+    abort(404)
+
+
+@app_views.route('/reviews/<review_id>', methods=['DELETE'],
                  strict_slashes=False)
-def all_amenities_by_place(place_id):
-    """ retrieve list of all Amenity objects """
-    place = storage.get('Place', place_id)
-    all_amenities = []
-    if not place:
-        abort(404)
-    for amenity in place.amenities:
-        all_amenities.append(amenity.to_dict())
-    return jsonify(all_amenities)
+def delete_review(review_id):
+    """ delete a Review """
+    review = storage.get('Review', review_id)
+    if review:
+        storage.delete(review)
+        storage.save()
+        return {}
+    abort(404)
 
 
-@app_views.route('/api/v1/places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE'], strict_slashes=False)
-def delete_amenity_by_place(place_id, amenity_id):
-    """ delete an Amenity """
-    place = storage.get('Place', place_id)
-    if not place:
+@app_views.route('/places/<place_id>/reviews', methods=['POST'],
+                 strict_slashes=False)
+def create_review(place_id):
+    """ create a Review """
+    review_name = request.get_json()
+    if not storage.get('Place', place_id):
         abort(404)
-    amenity = storage.get('Amenity', amenity_id)
-    if not amenity:
+    if not review_name:
+        abort(400, {'Not a JSON'})
+    elif 'user_id' not in review_name:
+        abort(400, {'Missing user_id'})
+    elif not storage.get('User', review_name['user_id']):
         abort(404)
-    if amenity not in place.amenities:
-        abort(404)
-    place.amenities.remove(amenity)
+    elif 'text' not in review_name:
+        abort(400, {'Missing text'})
+    review_name['place_id'] = place_id
+    new_review = Review(**review_name)
+    storage.new(new_review)
     storage.save()
-    return {}
+    return new_review.to_dict(), 201
 
 
-@app_views.route('/api/v1/places/<place_id>/amenities/<amenity_id>',
-                 methods=['POST'], strict_slashes=False)
-def link_amenity_to_place(place_id, amenity_id):
-    """ link an Amenity to a Place """
-    place = storage.get('Place', place_id)
-    if not place:
+@app_views.route('/reviews/<review_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_review(review_id):
+    """ update a Review """
+    update_attr = request.get_json()
+    if not update_attr:
+        abort(400, {'Not a JSON'})
+    my_review = storage.get('Review', review_id)
+    if not my_review:
         abort(404)
-    amenity = storage.get('Amenity', amenity_id)
-    if not amenity:
-        abort(404)
-    if amenity in place.amenities:
-        return amenity.to_dict()
-    place.amenities.append(amenity)
+    for key, value in update_attr.items():
+        if key not in ['id', 'user_id', 'place_id', 'created_at',
+                       'updated_at']:
+            setattr(my_review, key, value)
     storage.save()
-return amenity.to_dict(), 201
+    return my_review.to_dict()
